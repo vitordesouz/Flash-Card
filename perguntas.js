c6ed9261d35653dc0ab876be13190048b81f693b
odair <146240502+vitordesouz@users.noreply.github.com>
2024-10-21 10:40:26 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -3,16 +3,19 @@ criaCartao(
     'Qual é a principal raça de gado leiteiro no Brasil?'
     'A principal raça de gado leiteiro no Brasil é a Holandesa, conhecida pela sua alta produção de leite.'
 )
+
 criaCartao(
     'Programação',
     'Quais são os principais produtos derivados do leite?'
     'Os principais produtos derivados do leite incluem queijo, iogurte, manteiga e leite condensado.'
 )
+
 criaCartao(
     'Programação',
     'Qual é a importância da alimentação na produção de leite?'
     'A alimentação adequada é fundamental para a saúde do gado e diretamente influencia a quantidade e a qualidade do leite produzido.'
 )
+
 criaCartao(
     'Programação',
     'Qual é a importância da alimentação na produção de leite?'
