f08814db28d679a8871cbddb66e63b1b57f89221
odair <146240502+vitordesouz@users.noreply.github.com>
2024-10-21 10:50:39 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,60 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Pecuária Leiteira',
+    'Qual é a principal raça de gado leiteiro no Brasil?'
+    'A principal raça de gado leiteiro no Brasil é a Holandesa, conhecida pela sua alta produção de leite.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Quais são os principais produtos derivados do leite?',
+    'Os principais produtos derivados do leite incluem queijo, iogurte, manteiga e leite condensado.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Pecuária Leiteira',
+    'Qual é a importância da alimentação na produção de leite?',
+    'A alimentação adequada é fundamental para a saúde do gado e diretamente influencia a quantidade e a qualidade do leite produzido.'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Pecuária Leiteira',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Pecuária Leiteira',
+    'O que é ordenha e como é feita?',
+    'A ordenha é o processo de retirar o leite das vacas. Pode ser feita manualmente ou com ordenhadeiras mecânicas, geralmente duas vezes ao dia'
+
+)
+
+criaCartao(
+    'Pecuária Leiteira',
+    'Quais são os principais desafios da pecuária leiteira?',
+    'Os principais desafios incluem doenças do gado, variações climáticas, manejo adequado e a necessidade de investimento em tecnologia.'
+)
+
+criaCartao(
+    'Pecuária Leiteira',
+    'Como a qualidade do leite é avaliada??',
+    'A qualidade do leite é avaliada por meio de testes de gordura, proteína, contagem de células somáticas e presença de contaminantes.'
+)
+
+criaCartao(
+     'Pecuária Leiteira',
+    'Qual é a difere
+    'Lnça entre leite pasteurizado e leite cru?',
+    'O leite pasteurizado passa por um processo de aquecimento para eliminar bactérias, enquanto o leite cru é o leite que não foi tratado, mantendo todas as suas propriedades naturais.'
+)
+criaCartao(
+    'Pecuária Leiteira',
+    'Quais são os benefícios do leite para a saúde?',
+    'O leite é rico em cálcio, proteínas, vitaminas e minerais, contribuindo para a saúde óssea, muscular e do sistema imunológico.'
+)
+
+criaCartao(
+    'Pecuária Leiteira',
+    'Como a sustentabilidade está sendo incorporada na pecuária leiteira?',
+    'A sustentabilidade na pecuária leiteira é promovida por meio de práticas de manejo eficiente, uso de tecnologias que reduzem a emissão de gases e programas de bem-estar animal.'
+
 )
